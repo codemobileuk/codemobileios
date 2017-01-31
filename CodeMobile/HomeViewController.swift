53df5c48bebe26f02493bc2f30575f1cf51d8508
Louis Woods <louis@steves-macbook-pro.local>
2017-01-31 15:10:08 +0000

    Home screen redesign

--- CodeMobile/HomeViewController.swift
+++ CodeMobile/HomeViewController.swift
@@ -10,47 +10,49 @@ import UIKit
 
 class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
+
+    @IBOutlet weak var tweetsCollectionView: UICollectionView!
     @IBOutlet weak var scheduleCollectionView: UICollectionView!
     @IBOutlet weak var scheduleView: UIView!
+    @IBOutlet weak var bannerBackground: UIView!
     
     override func viewWillAppear(_ animated: Bool) {
         
         tabBarController?.navigationItem.title = "Home"
         tabBarController?.navigationItem.rightBarButtonItem = nil
-    }
-    @IBAction func switchToSpeakerTab(_ sender: Any) {
-        
-        print("Speaker tab btn tapped")
-        tabBarController?.selectedIndex = 2
-    }
-    
-    @IBAction func switchToScheduleTab(_ sender: Any) {
-        
-        print("Schedule tab btn tapped")
-        tabBarController?.selectedIndex = 1
-    }
-    
-    @IBAction func switchToMapTab(_ sender: Any) {
-        
-        print("Map tab btn tapped")
-        tabBarController?.selectedIndex = 3
+         
     }
     
     //  MARK: Collection View Functions
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 4
+        return 6
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = scheduleCollectionView.dequeueReusableCell(withReuseIdentifier: "CurrentlyOn", for: indexPath)
+        
+        if collectionView == scheduleCollectionView {
+        let cell = scheduleCollectionView.dequeueReusableCell(withReuseIdentifier: "CurrentlyOn", for: indexPath)
+        return cell
+        }
+        else if collectionView == tweetsCollectionView {
+            
+            let cell = tweetsCollectionView.dequeueReusableCell(withReuseIdentifier: "TweetCell", for: indexPath)
+            return cell
+
+        }
+        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        return CGSize(width: scheduleCollectionView.frame.size.width/2 - 2.5, height: scheduleView.frame.size.height)
+        if collectionView == tweetsCollectionView {
+            return CGSize(width: 320 , height: 80)
+        }
+        return CGSize(width: 170 , height: scheduleCollectionView.frame.size.height)
     }
 }
