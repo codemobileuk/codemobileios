1981756d7296df60971684353d7913344b231950
PDA Monkey <mail@pdamonkey.com>
2019-03-29 08:31:12 +0000

    Correct date formatting issue

--- CodeMobile/NSDateExtensions.swift
+++ CodeMobile/NSDateExtensions.swift
@@ -9,30 +9,24 @@
 import Foundation
 
 extension Date {
-    
     func isBetweeen(date date1: Date, andDate date2: Date) -> Bool {
-        
         return date1.compare(self as Date) == self.compare(date2 as Date)
     }
     
     func formatDate(dateToFormat: String) -> Date {
-        
         let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        let formattedDate = dateFormatter.date(from: dateToFormat)
+        
+        if formattedDate != nil { return formattedDate! }
+        
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        let formattedDate = dateFormatter.date(from: dateToFormat )
+        dateFormatter.locale = NSLocale(localeIdentifier:"en_US_POSIX") as Locale // This fixes bug when using 12 hr clock
         
-        if formattedDate == nil {
-            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-            dateFormatter.locale = NSLocale(localeIdentifier:"en_US_POSIX") as Locale! // This fixes bug when using 12 hr clock
-            let formattedDate = dateFormatter.date(from: dateToFormat )
-            
-            return formattedDate!
-        }
-        return formattedDate!
-        
+        return dateFormatter.date(from: dateToFormat) ?? Date()
     }
+    
     func wordedDate(Date: Date) -> String{
-        
         let formatter = DateFormatter()
         formatter.dateFormat = "h:mm a 'on' MMMM dd"
         formatter.amSymbol = "AM"
